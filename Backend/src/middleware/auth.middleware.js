7b29cd7f84daf7a5f2fedbb272d4f20b5eeb103e
Adnan <adnanshakir780@gmail.com>
2026-03-02 00:40:09 +0530

    Add redis-parser and standard-as-callback modules
    - Introduced redis-parser for high-performance Redis protocol parsing.
    - Included standard-as-callback for promise to callback conversion.
    - Configured Redis connection in cache.js with error handling.

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,8 +1,20 @@
+const blackListModel = require("../models/blacklist.model");
 const userModel = require("../models/user.model");
+const redis = require("../config/cache");
 const jwt = require("jsonwebtoken");
 
 const authUser = async (req, res, next) => {
   const token = req.cookies.token;
+
+  const isTokenBlacklisted = await blackListModel.findOne({
+    token
+  })
+
+  if(isTokenBlacklisted){
+    return res.status(401).json({
+      message: "Token is invalid",
+    });
+  }
 
   if (!token) {
     return res.status(401).json({
