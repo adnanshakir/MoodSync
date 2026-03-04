6ab68a53654949d99404f50b958f3e39164c9307
Adnan <adnanshakir780@gmail.com>
2026-03-05 01:52:26 +0530

    feat: enhance song upload functionality; add mood field and optimize file uploads; fix albumArtUrl typo; implement storage service

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -1,9 +1,39 @@
 const songModel = require("../models/song.model");
+const storageService = require("../services/storage.service");
 const id3 = require("node-id3");
 
 async function uploadSong(req, res) {
-  const tags = id3.read(req.file.buffer);
-  console.log(tags);
+  const songBuffer = req.file.buffer;
+  const { mood } = req.body;
+  const tags = id3.read(songBuffer);
+
+  //  Optimized instead of uploading file separately, Now we are uploading them simultaneously
+
+  const [songFile, albumArt] = await Promise.all([
+    storageService.uploadFile({
+      buffer: songBuffer,
+      filename: tags.title + ".mp3",
+      folder: "/mood-sync/songs",
+    }),
+
+    storageService.uploadFile({
+      buffer: tags.image.imageBuffer,
+      filename: tags.title + ".jpeg",
+      folder: "/mood-sync/albumArt",
+    }),
+  ]);
+
+  const song = await songModel.create({
+    title: tags.title,
+    url: songFile.url,
+    albumArtUrl: albumArt.url,
+    mood,
+  });
+
+  res.status(201).json({
+    message: "Song added succesfully",
+    song,
+  });
 }
 
 module.exports = { uploadSong };
